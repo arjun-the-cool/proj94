95f31c30dc08f317b43e6dc69806e1c3dffb8703
arjun-the-cool <80453159+arjun-the-cool@users.noreply.github.com>
2021-09-12 19:14:01 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCWswHnFt2U4zKvQ51ZQsX-QOMBUi4tkw0",
+    authDomain: "lets-chat-web-app-f89fb.firebaseapp.com",
+    projectId: "lets-chat-web-app-f89fb",
+    storageBucket: "lets-chat-web-app-f89fb.appspot.com",
+    messagingSenderId: "292664086771",
+    appId: "1:292664086771:web:5fbeb7fc2680a602462786"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
